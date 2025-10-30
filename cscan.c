93261ec9cd675bc67c1c303610d30699077c324d
amruthavvv <166039036+amruthavvv@users.noreply.github.com>
2025-10-31 00:18:24 +0530

    Update cscan.c

--- cscan.c
+++ cscan.c
@@ -4,26 +4,22 @@
 void main() {
     int n, i, j, temp, head, size;
     int tot_seek = 0;
-    
+
     printf("Enter the number of disk requests: ");
     scanf("%d", &n);
-    
-  
-    
+
+    int req[n + 10];
     printf("Enter the disk request sequence: \n");
     for (i = 0; i < n; i++) {
         scanf("%d", &req[i]);
     }
-    
+
     printf("Enter initial head position: ");
     scanf("%d", &head);
-    
+
     printf("Enter total disk size: ");
     scanf("%d", &size);
-    
-    
-    
-    
+
     for (i = 0; i < n - 1; i++) {
         for (j = 0; j < n - 1 - i; j++) {
             if (req[j] > req[j + 1]) {
@@ -35,57 +31,50 @@ void main() {
     }
 
     int head_index = 0;
-    // Find the position of the initial head in the sorted array
     for (i = 0; i < n; i++) {
-        if (req[i] == head) {
+        if (req[i] >= head) {
             head_index = i;
             break;
         }
     }
 
     int serviced[n + 10], seek[n + 10], c = 0, current_head = head;
-    
-    // --- FIRST PASS: Moving Up (towards size-1) and servicing requests ---
-    // Start from the request after the head position
-    for (i = head_index + 1; i < n; i++) {
+
+    for (i = head_index; i < n; i++) {
         seek[c] = abs(current_head - req[i]);
         serviced[c] = req[i];
         tot_seek += seek[c];
         current_head = req[i];
         c++;
     }
-  
-    int size_limit = size - 1;
-    if (current_head != size_limit) { 
-        seek[c] = abs(current_head - size_limit);
-        serviced[c] = size_limit;
+
+    if (current_head != size - 1) {
+        seek[c] = abs(current_head - (size - 1));
+        serviced[c] = size - 1;
         tot_seek += seek[c];
-        current_head = size_limit;
+        current_head = size - 1;
         c++;
     }
-    
-  
+
+    seek[c] = abs(current_head - 0);
     serviced[c] = 0;
     tot_seek += seek[c];
     current_head = 0;
     c++;
-    
-  
-    for (i = 0; i < head_index; i++) { 
+
+    for (i = 0; i < head_index; i++) {
         seek[c] = abs(current_head - req[i]);
         serviced[c] = req[i];
         tot_seek += seek[c];
         current_head = req[i];
         c++;
     }
-    
-  
+
     printf("\nRequest\tSeek time\n");
     for (i = 0; i < c; i++) {
         printf("%d\t%d\n", serviced[i], seek[i]);
     }
 
     printf("\nTotal Seek time: %d\n", tot_seek);
-    // n-1 is the original number of requests
-    printf("\nAverage seek time: %.2f\n", (float)tot_seek / (n - 1)); 
+    printf("Average Seek time: %.2f\n", (float)tot_seek / n);
 }

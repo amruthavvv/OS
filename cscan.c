433abff8d7a7212f5ba8f8448f0f8961cf13572f
amruthavvv <166039036+amruthavvv@users.noreply.github.com>
2025-11-02 22:10:26 +0530

    Update cscan.c

--- cscan.c
+++ cscan.c
@@ -1,80 +1,112 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<stdlib.h>
 
-void main() {
-    int n, i, j, temp, head, size;
-    int tot_seek = 0;
+void main()
+{
+    int n, i, j, temp, pos, rq[50], head, dir, size, total = 0;
 
-    printf("Enter the number of disk requests: ");
+    printf("Enter the number of requests: ");
     scanf("%d", &n);
 
-    int req[n + 10];
-    printf("Enter the disk request sequence: \n");
-    for (i = 0; i < n; i++) {
-        scanf("%d", &req[i]);
+    printf("Enter the requests:\n");
+    for(i = 0; i < n; i++)
+    {
+        printf("%d. ", i + 1);
+        scanf("%d", &rq[i]);
     }
 
-    printf("Enter initial head position: ");
+    printf("\nEnter initial head position: ");
     scanf("%d", &head);
 
-    printf("Enter total disk size: ");
+    printf("Enter the disk size: ");
     scanf("%d", &size);
 
-    for (i = 0; i < n - 1; i++) {
-        for (j = 0; j < n - 1 - i; j++) {
-            if (req[j] > req[j + 1]) {
-                temp = req[j];
-                req[j] = req[j + 1];
-                req[j + 1] = temp;
+    printf("0 = left, 1 = right\nEnter the direction: ");
+    scanf("%d", &dir);
+
+    rq[n] = head;
+    n++;
+
+    for(i = 0; i < n - 1; i++)
+    {
+        for(j = 0; j < n - i - 1; j++)
+        {
+            if(rq[j] > rq[j + 1])
+            {
+                temp = rq[j];
+                rq[j] = rq[j + 1];
+                rq[j + 1] = temp;
             }
         }
     }
 
-    int head_index = 0;
-    for (i = 0; i < n; i++) {
-        if (req[i] >= head) {
-            head_index = i;
+    for(i = 0; i < n; i++)
+    {
+        if(rq[i] == head)
+        {
+            pos = i;
             break;
         }
     }
 
-    int serviced[n + 10], seek[n + 10], c = 0, current_head = head;
+    printf("\nC-SCAN service order:\n");
+    printf("%d", head);
 
-    for (i = head_index; i < n; i++) {
-        seek[c] = abs(current_head - req[i]);
-        serviced[c] = req[i];
-        tot_seek += seek[c];
-        current_head = req[i];
-        c++;
+    if(dir == 1)
+    {
+        for(i = pos + 1; i < n; i++)
+        {
+            total += abs(head - rq[i]);
+            head = rq[i];
+            printf(" -> %d", head);
+        }
+
+        if(head != size - 1)
+        {
+            total += abs(head - (size - 1));
+            head = size - 1;
+            printf(" -> %d", head);
+        }
+
+        total += abs(head - 0);
+        head = 0;
+        printf(" -> %d", head);
+
+        for(i = 0; i < pos; i++)
+        {
+            total += abs(head - rq[i]);
+            head = rq[i];
+            printf(" -> %d", head);
+        }
+    }
+    else
+    {
+        for(i = pos - 1; i >= 0; i--)
+        {
+            total += abs(head - rq[i]);
+            head = rq[i];
+            printf(" -> %d", head);
+        }
+
+        if(head != 0)
+        {
+            total += abs(head - 0);
+            head = 0;
+            printf(" -> %d", head);
+        }
+
+        total += abs(head - (size - 1));
+        head = size - 1;
+        printf(" -> %d", head);
+
+        for(i = n - 1; i > pos; i--)
+        {
+            total += abs(head - rq[i]);
+            head = rq[i];
+            printf(" -> %d", head);
+        }
     }
 
-    if (current_head != size - 1) {
-        seek[c] = abs(current_head - (size - 1));
-        serviced[c] = size - 1;
-        tot_seek += seek[c];
-        current_head = size - 1;
-        c++;
-    }
-
-    seek[c] = abs(current_head - 0);
-    serviced[c] = 0;
-    tot_seek += seek[c];
-    current_head = 0;
-    c++;
-
-    for (i = 0; i < head_index; i++) {
-        seek[c] = abs(current_head - req[i]);
-        serviced[c] = req[i];
-        tot_seek += seek[c];
-        current_head = req[i];
-        c++;
-    }
-
-    printf("\nRequest\tSeek time\n");
-    for (i = 0; i < c; i++) {
-        printf("%d\t%d\n", serviced[i], seek[i]);
-    }
-
-    printf("\nTotal Seek time: %d\n", tot_seek);
-    printf("Average Seek time: %.2f\n", (float)tot_seek / n);
+    printf("\nTotal head movement : %d\n", total);
 }
+

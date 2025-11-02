11f878abb2c99f18dcbc9ec2d7f4ad1be4814d6b
amruthavvv <166039036+amruthavvv@users.noreply.github.com>
2025-11-02 20:11:28 +0530

    Update clook.c

--- clook.c
+++ clook.c
@@ -1,95 +1,94 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<stdlib.h>
 
-// Function to sort the request array
-void sort(int arr[], int n) {
-    int i, j, temp;
-    for (i = 0; i < n - 1; i++) {
-        for (j = 0; j < n - 1 - i; j++) {
-            if (arr[j] > arr[j + 1]) {
-                temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
+void main()
+{
+    int n, i, j, temp, pos, rq[50], head, dir, total = 0;
+
+    printf("Enter the number of requests: ");
+    scanf("%d", &n);
+
+    printf("Enter the requests:\n");
+    for(i = 0; i < n; i++)
+    {
+        printf("%d. ", i + 1);
+        scanf("%d", &rq[i]);
+    }
+
+    printf("\nEnter initial head position: ");
+    scanf("%d", &head);
+
+    printf("0 = left, 1 = right\nEnter the direction: ");
+    scanf("%d", &dir);
+
+    rq[n] = head;
+    n++;
+
+    for(i = 0; i < n - 1; i++)
+    {
+        for(j = 0; j < n - i - 1; j++)
+        {
+            if(rq[j] > rq[j + 1])
+            {
+                temp = rq[j];
+                rq[j] = rq[j + 1];
+                rq[j + 1] = temp;
             }
         }
     }
-}
 
-int main() {
-    int requests[100], n, head, direction;
-    int total_seek = 0, i, pos, curr;
-    
-    printf("Enter number of requests: ");
-    scanf("%d", &n);
-    
-    printf("Enter request sequence: \n");
-    for (i = 0; i < n; i++) {
-        scanf("%d", &requests[i]);
-    }
-    
-    printf("Enter initial head position: ");
-    scanf("%d", &head);
-    
-    // 0 for left/down, 1 for right/up
-    printf("Enter direction (0 for left, 1 for right): "); 
-    scanf("%d", &direction);
-    
-    requests[n] = head;
-    n++; 
-
-    sort(requests, n);
-
-    pos = -1;
-    for (i = 0; i < n; i++) {
-        if (requests[i] == head) {
+    for(i = 0; i < n; i++)
+    {
+        if(rq[i] == head)
+        {
             pos = i;
             break;
         }
     }
-    
-    curr = head;
-    
-    printf("\nC-LOOK Disk Scheduling\n");
-    printf("Request\tSeek Time\n");
-    
-    if (direction == 1) { // Moving Right (Up)
-        
-        // FIRST PASS: Moving Right and servicing requests
-        for (i = pos + 1; i < n; i++) {
-            int seek_val = abs(requests[i] - curr);
-            printf("%d\t%d\n", requests[i], seek_val);
-            total_seek += seek_val;
-            curr = requests[i];
+
+    printf("\nC-LOOK service order:\n");
+    printf("%d", head);
+
+    if(dir == 1)
+    {
+        for(i = pos + 1; i < n; i++)
+        {
+            total += abs(head - rq[i]);
+            head = rq[i];
+            printf(" -> %d", head);
         }
-        
-        // SECOND PASS: Jump to requests[0] and service remaining
-        for (i = 0; i < pos; i++) { 
-            int seek_val = abs(requests[i] - curr);
-            printf("%d\t%d\n", requests[i], seek_val);
-            total_seek += seek_val;
-            curr = requests[i];
-        }
-        
-    } else { // direction == 0 (Moving Left/Down)
-    
-        // FIRST PASS: Moving Left and servicing requests
-        for (i = pos - 1; i >= 0; i--) { 
-            int seek_val = abs(requests[i] - curr);
-            printf("%d\t%d\n", requests[i], seek_val);
-            total_seek += seek_val;
-            curr = requests[i];
-        }
-        
-        for (i = n - 1; i > pos; i--) { 
-            int seek_val = abs(requests[i] - curr);
-            printf("%d\t%d\n", requests[i], seek_val);
-            total_seek += seek_val;
-            curr = requests[i];
+
+        total += abs(head - rq[0]);
+        head = rq[0];
+        printf(" -> %d", head);
+
+        for(i = 1; i < pos; i++)
+        {
+            total += abs(head - rq[i]);
+            head = rq[i];
+            printf(" -> %d", head);
         }
     }
-    
-    printf("\nTotal seek time: %d\n", total_seek);
-    printf("Average seek time: %.2f\n", (float)total_seek / (n - 1));
-    
-    return 0;
+    else
+    {
+        for(i = pos - 1; i >= 0; i--)
+        {
+            total += abs(head - rq[i]);
+            head = rq[i];
+            printf(" -> %d", head);
+        }
+
+        total += abs(head - rq[n - 1]);
+        head = rq[n - 1];
+        printf(" -> %d", head);
+
+        for(i = n - 2; i > pos; i--)
+        {
+            total += abs(head - rq[i]);
+            head = rq[i];
+            printf(" -> %d", head);
+        }
+    }
+
+    printf("\nTotal head movement: %d\n", total);
 }
